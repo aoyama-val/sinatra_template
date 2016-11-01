96d10ec96df85c52934d5eb77801afa66973ab68
Aoyama, Shotaro <aosho235@gmail.com>
2016-11-02 00:43:52 +0900

    update

--- app.rb
+++ app.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+set :show_exceptions, false
+
+def required(params, key)
+  if not params.key?(key)
+    raise "parameter #{key} is required"
+  end
+  return params[key]
+end
+
+def optional(params, key, default=nil)
+  if params.key?(key)
+    return params[key]
+  else
+    return default
+  end
+end
+
+error do
+  'エラーが発生しました。 - ' + env['sinatra.error'].message
+end
+
 get '/' do
+  a = required(params, "a")
+  b = optional(params, "b", 100)
   puts "called"
-  'Hello, World !'
+  "a = #{a} b = #{b}"
 end
