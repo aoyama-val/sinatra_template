716400d2c0011c69e083eee51628a6cf6df8d89c
Aoyama, Shotaro <aosho235@gmail.com>
2016-11-05 10:40:52 +0900

    fix

--- app.rb
+++ app.rb
@@ -1,16 +1,22 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require "sinatra"
+require "sinatra/reloader"
+require "json"
+#require "sqlite3"
 
 set :show_exceptions, false
 
-def required(params, key)
+# クエリーパラメータから必須項目を取り出す
+def get_required(params, key)
+  key = key.to_s
   if not params.key?(key)
-    raise "parameter #{key} is required"
+    raise "parameter #{key} is missing"
   end
   return params[key]
 end
 
-def optional(params, key, default=nil)
+# クエリーパラメータから任意項目を取り出す
+def get_optional(params, key, default=nil)
+  key = key.to_s
   if params.key?(key)
     return params[key]
   else
@@ -18,13 +24,28 @@ def optional(params, key, default=nil)
   end
 end
 
+# before
+before do
+  response.headers["Content-Type"] = "application/json; charset=utf-8"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+end
+
+# after
+after do
+end
+
 error do
   'エラーが発生しました。 - ' + env['sinatra.error'].message
 end
 
 get '/' do
-  a = required(params, "a")
-  b = optional(params, "b", 100).to_i
+  a = get_required(params, "a")
+  b = get_optional(params, "b", 100).to_i
   puts "called"
-  "a = #{a} b = #{b}"
+  ret = {
+    params: {}
+  }
+  ret[:params][:a] = a
+  ret[:params][:b] = b
+  return JSON.generate(ret)
 end
